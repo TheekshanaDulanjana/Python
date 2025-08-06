5b7435afc46606e0ac377f3aa0fd7b88c49d9126
OPT Dulanjana <140092772+TheekshanaDulanjana@users.noreply.github.com>
2025-08-06 12:58:47 +0530

    First Code

---
+++ main.py
@@ -0,0 +1,36 @@
+# print("Welcome to Python ")
+# print(7+3)
+# print(type(6.66))
+# print(10%2==0)
+# print("AI"+" Chatbot")
+
+# # i = 20
+# # if (i==10):
+# #     print("i is 10")
+    
+# # elif (i==15):
+# #     print("i is 15")
+# # elif (i==20):
+# #     print("i is 20")
+# # else:
+# #     print("i is not present")
+
+# i=1
+# while i<6:
+#     print(i)
+#     i += 1 
+    
+
+# fruits=["apple", "banana", "cherry"]
+# for x in fruits:
+#     print(x)
+
+
+
+marks=int(input("Enter number 01:"))
+marks2=int(input("Enter number 02:"))
+# if int(marks) >= 50:
+#     print("You have passed")    
+# else:
+#     print("You have failed")
+print(marks+marks2)
